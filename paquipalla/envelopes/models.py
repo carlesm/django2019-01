ac6b60763f486f3404b76ee40a1e39079f952fce
Carles Mateu <carlesm@carlesm.com>
2019-02-26 10:31:18 +0100

    __str__ on admin interface

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -5,7 +5,13 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Envelope(models.Model):
     amount = models.IntegerField()
     motive = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.company.name+" - "+self.motive
