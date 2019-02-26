854bd35c0a801b1a289842124c833637ef22cbef
Carles Mateu <carlesm@carlesm.com>
2019-02-26 10:16:30 +0100

    New models for envelope and company

--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+class Envelope(models.Model):
+    amount = models.IntegerField()
+    motive = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
